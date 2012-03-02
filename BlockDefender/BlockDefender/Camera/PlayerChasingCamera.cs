51f0a59586e23148f8c86ae72dbf6a57b4b69957
Jan Sandbrink <fischikowski@web.de>
2012-03-02 11:10:55 +0100

    chasing camera initialized at player-position (closes #9)

--- BlockDefender/BlockDefender/Camera/PlayerChasingCamera.cs
+++ BlockDefender/BlockDefender/Camera/PlayerChasingCamera.cs
@@ -21,14 +21,20 @@ namespace BlockDefender.Camera
             CenterTranslation = new Vector3(device.Viewport.Width / 2, device.Viewport.Height / 2, 0f);
             Player = player;
             Transformation = Matrix.CreateScale(ZoomFactor);
+            Transformation.Translation = calculateTargetTranslation();
         }
 
         public Matrix computeTransformation()
         {
-            Vector3 targetTranslation = Vector3.Multiply(new Vector3(-Player.Position, 0f), ZoomFactor) + CenterTranslation;
+            Vector3 targetTranslation = calculateTargetTranslation();
             Vector3 translationDifference = targetTranslation - Transformation.Translation;
             Transformation.Translation += translationDifference * 0.05f;
             return Transformation;
+        }
+
+        private Vector3 calculateTargetTranslation()
+        {
+            return Vector3.Multiply(new Vector3(-Player.Position, 0f), ZoomFactor) + CenterTranslation;
         }
     }
 }
